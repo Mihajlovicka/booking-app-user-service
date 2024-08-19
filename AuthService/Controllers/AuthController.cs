3cbf4d2df1ea774a2a1368d3b4342405fb4ee996
Sara <saramihajlovic94@gmail.com>
2024-08-20 01:07:18 +0200

    ﻿feature-testing: Create testing project with test for login and registration

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using AuthService.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AuthService.Model;
+using AuthService.Model.Dto;
+
+namespace AuthService.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        protected Response _response;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new();
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
+        { 
+            var errorMessage = await _authService.Register(model);
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                _response.IsSuccess = false;
+                _response.Message  = errorMessage;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            var loginResponse = await _authService.Login(model);
+            if (loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Username or password is incorrect";
+                return BadRequest(_response);
+            }
+
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+    }
+}
---
+++ AuthService/Migrations/20240824133308_AddRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AuthService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3aff2420-a665-49c5-aaf9-02a26b88950d", null, "HOST", "HOST" },
+                    { "e35007a2-c8f3-4ddd-afcd-8ecd6453ca3b", null, "GUEST", "GUEST" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3aff2420-a665-49c5-aaf9-02a26b88950d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e35007a2-c8f3-4ddd-afcd-8ecd6453ca3b");
+        }
+    }
+}
